018a2a8eaf3460e9e99c52721c415f75439bbc07
Aniruddha Das <aniruddha.das.852@gmail.com>
2022-08-12 21:50:21 +0530

    chore: Added dotenv

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+app.use(express.json())
 
 app.get('/kpi', (req, res) => {
     res.json({ registered: 1254, professionals: 40, surgeries: 4287, hospitals: 8642 })
 })
 
-app.listen(5000, () => {
-    console.log("Server Listening on Port 5000")
+app.listen(port, () => {
+    console.log(`Server Listening on Port ${port}`)
 })
 
