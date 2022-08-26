b4d709c9dc2c8f0282c047c9c8f3e15baa940564
Soumwadeep Guha <soumwadeep@gmail.com>
2022-08-26 17:19:20 +0530

    Fixed status

--- src/controllers/Ci.controller.js
+++ src/controllers/Ci.controller.js
@@ -2,25 +2,28 @@ const pool = require("../models/db");
 const { v4: uuidv4 } = require("uuid");
 
 const getCi = async (req, res) => {
-  if(req.query.id === undefined || req.params.id === "" || req.params.id === null){
+  if (
+    req.query.id === undefined ||
+    req.params.id === "" ||
+    req.params.id === null
+  ) {
     try {
       const Ci = await pool.query(`SELECT * FROM ci_child`);
       res.json(Ci.rows);
     } catch (err) {
       console.log(err);
     }
-   }else
-   {
+  } else {
     console.log(req.query.id);
     try {
-      const {id} = req.query;
+      const { id } = req.query;
       console.log(id);
       const Ci = await pool.query(`SELECT * FROM ci_child where id = $1`, [id]);
       res.json(Ci.rows);
     } catch (err) {
       console.log(err);
     }
-   }
+  }
 };
 
 const postCi = async (req, res) => {
@@ -29,8 +32,8 @@ const postCi = async (req, res) => {
     console.log(formData);
     const { name, dob, age, gender, state, email, degree, type } = formData;
     const newCi = await pool.query(
-      `INSERT INTO ci_child (id, name, dob, age, gender, state, email, degree, type) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-      [uuidv4(), name, dob, age, gender, state, email, degree, type]
+      `INSERT INTO ci_child (id, name, dob, age, gender, state, email, degree, type, status) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`,
+      [uuidv4(), name, dob, age, gender, state, email, degree, type, "pending"]
     );
     res.json(newCi.rows);
   } catch (err) {
